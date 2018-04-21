60fc2e6a9d4b3e6ef233f5a8d01a7a99ae8c5f12
Mark Hatchell <mhatchell@mac.com>
2018-04-21 12:40:45 -0400

    replace routes and menu bar and top bar with material version.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,7 +7,46 @@ import WavelengthComponent from "../WavelengthComponent/WavelengthComponent";
 import TankCircuitComponent from "../TankCircuitComponent/TankCircuitComponent";
 import BatteryRuntimeComponent from "../Battery/BatteryRuntimeComponent";
 import ResistorDividerComponent from "../ResistorDividerComponent/ResistorDividerComponent";
+import TopBar from "../TopBar/TopBar"
+import SideMenu from "../SideMenu/SideMenu"
 
+const Routes = [
+  {
+    path: '/ohms-law',
+    name: 'Ohms Law',
+    render: (handleChangeTitle) => (props)=><OhmsLawComponent onChangeTitle={handleChangeTitle} {...props} />,
+  },
+  {
+    path: '/wavelength',
+    name: 'Wavelength',
+    render: (handleChangeTitle) => (props)=><WavelengthComponent onChangeTitle={handleChangeTitle} {...props}/>,
+  },
+  {
+    path: '/tank-circuit',
+    name: 'Tank Circuit',
+    render: (handleChangeTitle) => (props)=><TankCircuitComponent onChangeTitle={handleChangeTitle} {...props} />,
+  },
+  {
+    path: '/battery-runtime',
+    name: 'Battery Runtime',
+    render: (handleChangeTitle) => (props)=><BatteryRuntimeComponent onChangeTitle={handleChangeTitle} {...props} />,
+  },
+  {
+    path: '/resistor-divider',
+    name: 'Resistor Divider',
+    render: (handleChangeTitle) => (props)=><ResistorDividerComponent onChangeTitle={handleChangeTitle} {...props} />,
+  },
+];
+
+function MakeRoutes(props) {
+  const {
+    onChangeTitle,
+  } = props;
+
+  return Routes.map((route) => {
+    return <Route key={route.path} path={route.path} render={route.render(onChangeTitle)} />
+  });
+}
 
 class App extends Component {
 
@@ -17,25 +56,36 @@ class App extends Component {
       menuOpen: false,
       title: 'Engineering Equations'
     };
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
   }
 
   render() {
+    const {
+      toggleDrawer,
+      handleChangeTitle,
+    } = this;
+
+    const {
+      menuOpen,
+    } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header" onClick={this.handleMenuOpenClick.bind(this)}>
-          <h1 className="App-title">{this.state.title}</h1>
-        </header>
-        <Menu menuOpen={this.state.menuOpen} onMenuItemClick={this.handleMenuItemClick.bind(this)}/>
+        <TopBar title={this.state.title} onToggleDrawer={toggleDrawer} />
+        <SideMenu onToggleDrawer={toggleDrawer} isOpen={menuOpen} routes={Routes} />
         <Switch>
-          <Route path="/ohms-law" render={(props)=><OhmsLawComponent onChangeTitle={(title) => {this.handleChangeTitle(title)}} {...props} /> } />
-          <Route path="/wavelength" render={(props)=><WavelengthComponent onChangeTitle={(title) => {this.handleChangeTitle(title)}} {...props}/> } />
-          <Route path="/tank-circuit" render={(props)=><TankCircuitComponent onChangeTitle={(title) => {this.handleChangeTitle(title)}} {...props} /> } />
-          <Route path="/battery-runtime" render={(props)=><BatteryRuntimeComponent onChangeTitle={(title) => {this.handleChangeTitle(title)}} {...props} />} />
-          <Route path="/resistor-divider" render={(props)=><ResistorDividerComponent onChangeTitle={(title) => {this.handleChangeTitle(title)}} {...props} />} />
+          <MakeRoutes onChangeTitle={handleChangeTitle} />
         </Switch>
       </div>
     );
   }
+
+  toggleDrawer() {
+    this.setState({
+      menuOpen: !this.state.menuOpen,
+    });
+  };
 
   handleChangeTitle(newTitle) {
     document.title = newTitle;
@@ -44,20 +94,6 @@ class App extends Component {
       {
         title: newTitle
       }
-    ));
-  }
-
-  handleMenuOpenClick() {
-    this.setState(Object.assign(
-      this.state,
-      {menuOpen: !this.state.menuOpen}
-    ));
-  }
-
-  handleMenuItemClick() {
-    this.setState(Object.assign(
-      this.state,
-      {menuOpen: false}
     ));
   }
 
