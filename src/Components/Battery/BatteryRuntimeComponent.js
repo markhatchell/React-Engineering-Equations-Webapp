6aa4272d5f376be0c6849036d8490f33f1c9e52e
Mark Hatchell <mhatchell@mac.com>
2018-04-22 09:00:51 -0400

    updated battery runtime component to material ui

--- src/Components/Battery/BatteryRuntimeComponent.js
+++ src/Components/Battery/BatteryRuntimeComponent.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import BaseEquation from '../BaseEquation';
 import '../../Styles/Equations.css';
+import Card, { CardContent, CardActions } from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+
 const Battery = require('js-electrical-engineering-equations').Battery;
 const getFloat = require('js-electrical-engineering-equations').getFloat;
 
@@ -25,99 +30,98 @@ class BatteryRuntimeComponent extends BaseEquation {
 
   render() {
     return (
-      <div className="equationBox">
-        <h3>Battery Runtime</h3>
-        <div className="equationBoxInner">
-          <p>Calculate the runtime on a battery. This equation does not take into account the diminished runtime effect of
-            high current applications.</p>
-          <p>The efficiency of the system is set to 80% by default.</p>
-          <div className="tableContainer">
-            <form onSubmit={(e) => this.handleSubmit(e)}>
-              <table className="table" width="100%">
-                <tbody>
-                <tr>
-                  <td align="left">
-                    <label htmlFor="batteryCapacityInAmpHours">Battery capacity in amp hours</label>
-                  </td>
-                  <td>
-                    <input id="batteryCapacityInAmpHours" value={this.state.batteryCapacityInAmpHours}
-                           autoComplete="off"
-                           onChange={(e) => this.collectFloatValueFor('batteryCapacityInAmpHours', e)}/>
-                  </td>
-                </tr>
-                <tr>
-                  <td align="left">
-                    <label htmlFor="currentDrawInAmps">Current draw in amps</label>
-                  </td>
-                  <td>
-                    <input id="currentDrawInAmps" value={this.state.currentDrawInAmps}
-                           autoComplete="off"
-                           onChange={(e) => this.collectFloatValueFor('currentDrawInAmps', e)}/>
-                  </td>
-                </tr>
-                <tr>
-                  <td align="left">
-                    <label htmlFor="efficiency">Efficiency of system</label>
-                  </td>
-                  <td>
-                    <input id="efficiency" value={this.state.efficiency}
-                           autoComplete="off"
-                           onChange={(e) => this.collectFloatValueFor('efficiency', e)}/>
-                  </td>
-                </tr>
-                <tr>
-                  <td align="left">
-                    Battery capacity in amp hours
-                  </td>
-                  <td align="left">
-                    {this.state.battery.getBatteryCapacityInAmpHours()}
-                  </td>
-                </tr>
-                <tr>
-                  <td align="left">
-                    Battery capacity in coulombs
-                  </td>
-                  <td align="left">
-                    {this.state.battery.getBatteryCapacityInCoulombs()}
-                  </td>
-                </tr>
-                <tr>
-                  <td align="left">
-                    Battery run time in min
-                  </td>
-                  <td align="left">
-                    {this.state.battery.getRunTimeInMin()}
-                  </td>
-                </tr>
-                <tr>
-                  <td align="left">
-                    Battery run time in hours
-                  </td>
-                  <td align="left">
-                    {this.state.battery.getRunTimeInHours()}
-                  </td>
-                </tr>
-                <tr>
-                  <td align="left">
-                    Battery run time in days
-                  </td>
-                  <td align="left">
-                    {getFloat(this.state.battery.getRunTimeInHours() / 24, 4)}
-                  </td>
-                </tr>
-                <tr>
-                  <td align="left">
-                  </td>
-                  <td align="right">
-                    <button type="submit">Calculate</button>
-                  </td>
-                </tr>
-                </tbody>
-              </table>
-            </form>
-          </div>
-        </div>
-      </div>
+      <Card className="equationBox">
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <CardContent className="equationBoxInner">
+            <p>Calculate the runtime on a battery. This equation does not take into account the diminished runtime
+              effect of
+              high current applications.</p>
+            <p>The efficiency of the system is set to 80% by default.</p>
+            <TextField
+              id="full-width"
+              label="Battery capacity in amp hours"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              fullWidth
+              name="batteryCapacityInAmpHours"
+              margin="normal"
+              onChange={(e) => this.collectFloatValueFor('batteryCapacityInAmpHours', e)}
+              value={this.state.batteryCapacityInAmpHours}
+            />
+            <TextField
+              id="full-width"
+              label="Current draw in amps"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              fullWidth
+              name="currentDrawInAmps"
+              margin="normal"
+              onChange={(e) => this.collectFloatValueFor('currentDrawInAmps', e)}
+              value={this.state.currentDrawInAmps}
+            />
+            <TextField
+              id="full-width"
+              label="Efficiency of system"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              fullWidth
+              name="efficiency"
+              margin="normal"
+              onChange={(e) => this.collectFloatValueFor('efficiency', e)}
+              value={this.state.efficiency}
+            />
+            <Table>
+              <TableRow>
+                <TableCell align="left">
+                  Battery capacity in amp hours
+                </TableCell>
+                <TableCell align="left">
+                  {this.state.battery.getBatteryCapacityInAmpHours()}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align="left">
+                  Battery capacity in coulombs
+                </TableCell>
+                <TableCell align="left">
+                  {this.state.battery.getBatteryCapacityInCoulombs()}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align="left">
+                  Battery run time in min
+                </TableCell>
+                <TableCell align="left">
+                  {this.state.battery.getRunTimeInMin()}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align="left">
+                  Battery run time in hours
+                </TableCell>
+                <TableCell align="left">
+                  {this.state.battery.getRunTimeInHours()}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align="left">
+                  Battery run time in days
+                </TableCell>
+                <TableCell align="left">
+                  {getFloat(this.state.battery.getRunTimeInHours() / 24, 4)}
+                </TableCell>
+              </TableRow>
+            </Table>
+          </CardContent>
+          <CardActions>
+            <Button type="submit" tabIndex="4" color="primary" variant="raised"
+                    style={{marginLeft: 'auto'}}>Calculate</Button>
+          </CardActions>
+        </form>
+      </Card>
     );
   }
 
